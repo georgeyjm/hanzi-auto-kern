439b36e7237f6263fa0b93580604e99fb36e3cc3
George Yu <yudumpling@gmail.com>
2023-08-26 01:42:01 +0900

    Add basic object parsing

---
+++ group-poc.py
@@ -0,0 +1,5 @@
+# Proof of concept for grouping of glyphs based on their shapes
+
+from utils import GlyphsFile
+
+glyphs_data = GlyphsFile('Mon Gara.glyphs')
--- utils.py
+++ utils.py
@@ -1,11 +1,78 @@
 from lark import Lark, Transformer
 
-filepath = 'Mon Gara.glyphs'
-with open(filepath) as f:
-    raw_content = f.read()
+
+class GlyphsFile:
+
+    def __init__(self, filepath):
+        self.masters = {}
+        self.glyphs = {}
+
+        self.raw_data = self._parse_raw_glyphs_file(filepath)
+        self._parse_json_into_objects()
+
+    def _parse_raw_glyphs_file(self, filepath):
+        with open(filepath) as f:
+            parsed = _glyphs_parser.parse(f.read())
+            return parsed
+
+    def _parse_json_into_objects(self):
+        # Masters
+        for master in self.raw_data['fontMaster']:
+            master_obj = Master(master['id'], master['name'])
+            self.masters[master['id']] = master_obj
+        
+        # Glyphs
+        for glyph in self.raw_data['glyphs']:
+            glyph_obj = Glyph(self, glyph['glyphname'], glyph.get('unicode')) # For e.g. .liga glyphs, there is no unicode
+            self.glyphs[glyph['glyphname']] = glyph_obj
+            for layer in glyph['layers']:
+                if 'shapes' not in layer: # Empty glyph
+                    layer_obj = GlyphLayer(glyph_obj, layer['layerId'], None, None, True, layer['width'], [])
+                else:
+                    is_default = 'associatedMasterId' not in layer
+                    master_id = layer.get('associatedMasterId', layer['layerId'])
+                    name = layer.get('name', self.masters[master_id].name)
+                    layer_obj = GlyphLayer(glyph_obj, layer['layerId'], name, master_id, is_default, layer['width'], layer['shapes'])
+                glyph_obj.layers.append(layer_obj)
 
 
-class Tree2Json(Transformer):
+class Glyph:
+
+    def __init__(self, file, name, unicode):
+        self.file = file
+        self.name = name
+        self.unicode = unicode
+        self.layers = []
+    
+    def __repr__(self):
+        return f'<Glyph "{self.name}">'
+
+
+class GlyphLayer:
+
+    def __init__(self, glyph, layer_id, name, master_id, is_default, width, shapes):
+        self.glyph = glyph
+        self.id = layer_id
+        self.name = name
+        self.master_id = master_id # self.master?
+        self.is_default = is_default
+        self.width = width
+        self.shapes = shapes
+    
+    def __repr__(self):
+        return f'<Layer "{self.name}" of Glyph "{self.glyph.name}">'
+
+
+class Master:
+
+    def __init__(self, master_id, name):
+        self.id = master_id
+        self.name = name
+        # TODO: metric values
+
+
+class _GlyphsTransformer(Transformer):
+    '''Transformer to parse Glyphs file into JSON.'''
 
     def string(self, s):
         (s,) = s
@@ -27,7 +94,7 @@ class Tree2Json(Transformer):
     dict = dict
 
 
-glyphs_parser = Lark(r'''
+_glyphs_parser = Lark(r'''
     ?value : dict
            | list
            | string
@@ -45,6 +112,4 @@ glyphs_parser = Lark(r'''
     %import common.WS
     %ignore WS
 
-    ''', start='value', parser='lalr', transformer=Tree2Json())
-
-parsed = glyphs_parser.parse(raw_content)
+    ''', start='value', parser='lalr', transformer=_GlyphsTransformer())
